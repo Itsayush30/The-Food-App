a65295bda8cf56fca1733de3547b52c56a7ef68d
itsayush30 <itsayush30@gmail.com>
2023-09-29 21:28:18 +0530

    Fix Fooditem import

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import FoodItem from "./FoodItem";
+import FoodItem from "./Fooditem";
 import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
